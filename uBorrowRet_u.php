623379ca76f8ed4336c9a8e091b681664dec5a29
Koricsss <ecuyco25@gmail.com>
2025-05-14 03:11:49 +0800

    Update uBorrowRet_u.php

--- uBorrowRet_u.php
+++ uBorrowRet_u.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="js/request./js"></script>
+  <script src="js/request.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="uBorrowRet_u.css">
 </head>
