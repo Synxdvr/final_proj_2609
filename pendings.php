c4e1067b266ffc35d7fd85d3d866b3883f479d48
Mamond15 <raymondkarlbelza0921@gmail.com>
2025-05-13 20:04:22 +0800

    Create pendings.php

---
+++ pendings.php
@@ -0,0 +1,29 @@
+<?php include("db_connection.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <h1 class="text-center  text-white bg-dark col-md-12">PENDING LIST</h1>
+    <table class="table table-bordered col-md-12">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+	 <th scope="col">SUBJECT</th>
+	 <th scope="col">CONTENT</th>
+	 <th scope="col">STATUS</th>
+	 <th scope="col">ACTION</th>
+    </tr>
+  </thead>
+
+<?php 
+</body>
+</html>
